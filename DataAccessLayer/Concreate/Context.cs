3c5664d86f7416195fa273067df088f09cf24c74
osmanbas34 <osmanbas34@hotmail.com>
2022-05-17 20:33:18 +0300

    Add project files.

---
+++ DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concreate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=127.0.0.1;Port=5432;Database=myBlog;User Id=postgres;Password=a77ecef7;");
+        }
+        public DbSet<About>  Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
---
+++ OgrenciNotMVC/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgrenciNotMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
