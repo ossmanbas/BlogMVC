3a9c2bb8087f10ff0b1d9637d6c41cf539498fab
osmanbas34 <osmanbas34@hotmail.com>
2022-05-25 16:52:26 +0300

    11

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void AddBlog(Blog blog);
+        void RemoveBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+
+        List<Blog> GetList();
+        Blog GetCategoryById(int id);
+    }
+}
---
+++ BusinessLayer/Concreate/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concreate
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDAL _blogDAL;
+
+        public BlogManager(IBlogDAL blogDAL)
+        {
+            _blogDAL = blogDAL;
+        }
+
+        public void AddBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDAL.GetAll();
+        }
+
+        public void RemoveBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
