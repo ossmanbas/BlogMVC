e56c1c500951ab082fa4c3ec70757dc0d72388c9
Osman Baş <83305672+ossmanbas@users.noreply.github.com>
2022-06-25 17:21:00 +0300

    Dashboard Tamamlandı

--- MvcLayer/Controllers/DashboardController.cs
+++ MvcLayer/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcLayer.Controllers
 {
@@ -6,6 +7,10 @@ namespace MvcLayer.Controllers
     {
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
